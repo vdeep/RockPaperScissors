3572387c3fbdb07a383e5ae193c970dfd15d5585
Vishal Kanojia <vishal.deep@live.com>
2022-03-03 21:15:36 +0530

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Vishal on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
